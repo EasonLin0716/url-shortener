1ac1803753517c42f53583f3afe5f38d86fd5f2d
Eason Lin <uu9924079@gmail.com>
2019-10-08 10:45:37 +0800

    init project

---
+++ app.js
@@ -0,0 +1,35 @@
+/* -----require needed middlewares and others----- */
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const bodyParser = require('body-parser')
+
+/* -----db connecting----- */
+mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+/* -----middleware setting----- */
+app.use(express.static('public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(flash())
+
+/* -----route setting----- */
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('app.js is running')
+})
