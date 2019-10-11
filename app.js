ae9e2276e0c302be890210646d7f61791cd963f9
Eason Lin <uu9924079@gmail.com>
2019-10-11 19:22:59 +0800

    little fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ db.once('open', () => {
 const Urls = require('./models/urls')
 let numOfShortUrlUsed = 0
 Urls.find({}, (err, urls) => {
-  numOfShortUrlUsed = urls.length
+  numOfShortUrlUsed += urls.length
 })
 
 /* -----middleware setting----- */
